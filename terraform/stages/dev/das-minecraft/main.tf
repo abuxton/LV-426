7aa58eef9ba7aacc69e6b26eb38f76ab1c1d78cf
Adam Buxton <buxton.adam@gmail.com>
2020-04-11 19:34:23 +0100

    updates for mc

--- terraform/stages/dev/das-minecraft/main.tf
+++ terraform/stages/dev/das-minecraft/main.tf
@@ -4,30 +4,32 @@ terraform {
 }
 locals {
   key_name      = "tg_user"
-  bucket_id     = "lv-426-minecraft"
+  bucket_id     = "lv-426-minecraft-bucket"
   vpc_id        = "vpc-6ab4cd02"
   subnet_id     = "subnet-e64cc39c"
   mc_version    = "1.15.2" 
+  mc_root       = "/opt/minecraft"
 }
 
-resource "aws_s3_bucket" "minecraft-bucket" {
+resource "aws_s3_bucket" "b" {
   bucket = local.bucket_id
   acl    = "private"
-
+  region = "eu-west-2"
   tags = {
     Name        = local.bucket_id
     Environment = "Dev"
   }
 }
 
+
 module "minecraft" {
   source = "git@github.com:abuxton/terraform-aws-minecraft.git?ref=master"
-  module_depends_on = ["${module.minecraft.minecraft-bucket}]
+  module_depends_on = ["aws_s3_bucket.${local.bucket_id}"]
   key_name      = local.key_name
   bucket_id     = local.bucket_id
   vpc_id        = local.vpc_id
   subnet_id     = local.subnet_id
   mc_version    = local.mc_version
-
+  mc_root       = local.mc_root
 }
 
