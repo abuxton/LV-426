21691ab00f2656cc312d1cd80e43f14d49e0c837
Adam Buxton <buxton.adam@gmail.com>
2020-05-01 10:13:41 +0100

    fmt

--- terraform/stages/dev/das-minecraft/main.tf
+++ terraform/stages/dev/das-minecraft/main.tf
@@ -3,12 +3,12 @@ terraform {
   backend "s3" {}
 }
 locals {
-  key_name      = "tg_user"
-  bucket_id     = "lv-426-minecraft-bucket"
-  vpc_id        = "vpc-6ab4cd02"
-  subnet_id     = "subnet-e64cc39c"
-  mc_version    = "1.15.2" 
-  mc_root       = "/opt/minecraft/vanilla"
+  key_name   = "tg_user"
+  bucket_id  = "lv-426-minecraft-bucket"
+  vpc_id     = "vpc-6ab4cd02"
+  subnet_id  = "subnet-e64cc39c"
+  mc_version = "1.15.2"
+  mc_root    = "/opt/minecraft/vanilla"
 }
 
 resource "aws_s3_bucket" "b" {
@@ -23,12 +23,12 @@ resource "aws_s3_bucket" "b" {
 
 
 module "minecraft" {
-  source = "git@github.com:abuxton/terraform-aws-minecraft.git?ref=master"
-  key_name      = local.key_name
-  bucket_id     = local.bucket_id
-  vpc_id        = local.vpc_id
-  subnet_id     = local.subnet_id
-  mc_version    = local.mc_version
-  mc_root       = local.mc_root
+  source     = "git@github.com:abuxton/terraform-aws-minecraft.git?ref=master"
+  key_name   = local.key_name
+  bucket_id  = local.bucket_id
+  vpc_id     = local.vpc_id
+  subnet_id  = local.subnet_id
+  mc_version = local.mc_version
+  mc_root    = local.mc_root
 }
 
