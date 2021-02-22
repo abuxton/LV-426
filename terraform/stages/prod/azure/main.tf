b230c02d182ec1adb21ff83d9b98e64ca09d9bb0
Adam Buxton <buxton.adam@gmail.com>
2021-02-22 14:40:40 +0000

    add bastion

--- terraform/stages/prod/azure/main.tf
+++ terraform/stages/prod/azure/main.tf
@@ -96,3 +96,24 @@ data "azurerm_public_ip" "ip" {
   resource_group_name = azurerm_virtual_machine.vm.resource_group_name
   depends_on          = [azurerm_virtual_machine.vm]
 }
+module "azure-bastion" {
+  source  = "kumarvna/azure-bastion/azurerm"
+  version = "1.0.0"
+
+  # Resource Group, location, VNet and Subnet details
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+
+  # Azure bastion server requireemnts
+  azure_bastion_service_name          = "${prefix}-Bastion-Service"
+  azure_bastion_subnet_address_prefix = ["10.42.2.0/26"]
+
+  # Adding TAG's to your Azure resources (Required)
+  tags = {
+    ProjectName  = "demo-internal"
+    Env          = "dev"
+    Owner        = "user@example.com"
+    BusinessUnit = "CORP"
+    ServiceClass = "Gold"
+  }
+}
