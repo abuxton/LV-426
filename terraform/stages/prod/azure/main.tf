99c6b293a47d4f18517f30b4ef182a021f178bc6
Adam Buxton <buxton.adam@gmail.com>
2021-02-23 12:18:59 +0000

    bug

--- terraform/stages/prod/azure/main.tf
+++ terraform/stages/prod/azure/main.tf
@@ -1,35 +1,35 @@
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "new_rg" {
   name     = "${var.prefix}-rg"
   location = var.location
 }
-resource "azurerm_virtual_network" "myvnet" {
+resource "azurerm_virtual_network" "new_vnet" {
   name                = "${var.prefix}-VNet"
   address_space       = ["10.42.0.0/16"]
   location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = azurerm_resource_group.new_rg.name
 }
 # Create subnet
-resource "azurerm_subnet" "subnet" {
+resource "azurerm_subnet" "new_subnet" {
   name                 = "${var.prefix}-Subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = azurerm_resource_group.new_rg.name
+  virtual_network_name = azurerm_virtual_network.new_vnet.name
   address_prefixes     = ["10.42.1.0/24"]
 }
 
 # Create public IP
-resource "azurerm_public_ip" "publicip" {
+resource "azurerm_public_ip" "new_publicip" {
   name                = "${var.prefix}-PublicIP"
   location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = azurerm_resource_group.new_rg.name
   allocation_method   = "Static"
 }
 
 
 # Create Network Security Group and rule
-resource "azurerm_network_security_group" "nsg" {
+resource "azurerm_network_security_group" "new_nsg" {
   name                = "${var.prefix}-NSG"
   location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = azurerm_resource_group.new_rg.name
 
   security_rule {
     name                       = "SSH"
@@ -45,25 +45,25 @@ resource "azurerm_network_security_group" "nsg" {
 }
 
 # Create network interface
-resource "azurerm_network_interface" "nic" {
+resource "azurerm_network_interface" "new_nic" {
   name                = "${var.prefix}-NIC"
   location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = azurerm_resource_group.new_rg.name
 
   ip_configuration {
     name                          = "${var.prefix}-NICconfig"
-    subnet_id                     = azurerm_subnet.subnet.id
+    subnet_id                     = azurerm_subnet.new_subnet.id
     private_ip_address_allocation = "dynamic"
-    public_ip_address_id          = azurerm_public_ip.publicip.id
+    public_ip_address_id          = azurerm_public_ip.new_publicip.id
   }
 }
 
 # Create a Linux virtual machine
-resource "azurerm_virtual_machine" "vm" {
+resource "azurerm_virtual_machine" "new_vm" {
   name                  = "${var.prefix}-VM"
   location              = var.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.nic.id]
+  resource_group_name   = azurerm_resource_group.new_rg.name
+  network_interface_ids = [azurerm_network_interface.new_nic.id]
   vm_size               = "Standard_DS1_v2"
 
   storage_os_disk {
@@ -91,15 +91,15 @@ resource "azurerm_virtual_machine" "vm" {
   }
 }
 # create a windows vm
-resource "azurerm_windows_virtual_machine" "wvm" {
+resource "azurerm_windows_virtual_machine" "new_wvm" {
   name                = "example-machine"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.new_rg.name
+  location            = azurerm_resource_group.new_rg.location
   size                = "Standard_F2"
   admin_username      = var.admin_username
   admin_password      = var.admin_password
   network_interface_ids = [
-    azurerm_network_interface.nic.id,
+    azurerm_network_interface.new_nic.id,
   ]
 
   os_disk {
@@ -115,10 +115,10 @@ resource "azurerm_windows_virtual_machine" "wvm" {
   }
 }
 
-data "azurerm_public_ip" "ip" {
-  name                = azurerm_public_ip.publicip.name
-  resource_group_name = azurerm_virtual_machine.vm.resource_group_name
-  depends_on          = [azurerm_virtual_machine.vm]
+data "azurerm_public_ip" "new_ip" {
+  name                = azurerm_public_ip.new_publicip.name
+  resource_group_name = azurerm_virtual_machine.new_vm.resource_group_name
+  depends_on          = [azurerm_virtual_machine.new_vm]
 }
 module "azure-bastion" {
   source = "kumarvna/azure-bastion/azurerm"
@@ -126,8 +126,8 @@ module "azure-bastion" {
   version = "1.0.0"
 
   # Resource Group, location, VNet and Subnet details
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = azurerm_resource_group.new_rg.name
+  virtual_network_name = azurerm_virtual_network.new_vnet.name
 
   # Azure bastion server requireemnts
   azure_bastion_service_name          = "${var.prefix}-Bastion-Service"
